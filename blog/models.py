5eef8eb91fd6eb9b5fe999bb5cdb7716205c9242
AndreQ091 <vea86vvkoeyl@mail.ru>
2020-10-20 19:22:15 +0300

    Blog page updated

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blog')
